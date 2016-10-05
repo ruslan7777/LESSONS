20e8d347f4d2c48a0c5352c3de79ddc9d75c60b6
RUSLAN77 <ruslan01234567890123@gmail.com>
2016-10-05 10:34:55 +0300

    Rejected commit: Default

--- src/com/ConsDemo.java
+++ src/com/ConsDemo.java
@@ -10,7 +10,7 @@ class MyClass {
         x = 10;
     }
 }
-public class ConsDerno {
+public class ConsDemo {
     public static void main(String[] args) {
         MyClass t1 = new MyClass();
         MyClass t2 = new MyClass();
