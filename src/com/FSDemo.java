24466f8542e6849c410b89b4dfba0f35a67e776d
RUSLAN77 <ruslan01234567890123@gmail.com>
2016-11-08 20:31:54 +0200

    08.11.2016

---
+++ src/com/FSDemo.java
@@ -0,0 +1,75 @@
+package com;
+
+/**
+ * Created by RUSLAN77 on  08.11.2016 in Ukraine
+ * В этом классе реализуется "отказоустойчивый" массив, предотвращающий ошибки времени выполнения
+ */
+
+class FailSoftArray {
+    private int a[]; //ссылка на массив
+    private int errval; //значение возвращаемое в случае возникновения ошибки при выполнении метода get()
+    public int length; // открытая переменная length
+
+    /* Конструктору данного класса передается размер массива и значение, которое должен возвращать метод get() при
+возникновении ошибки. */
+    public FailSoftArray(int size, int errv) {
+        a = new int[size];
+        errval = errv;
+        length = size;
+    }
+
+    // Возвратить значение элемента массива с заданным индексом
+    public int get(int index) {
+        if (indexOK(index)) return a[index];               //Отслеживание попытки выхода  за границы массива
+        return errval;
+            }
+
+    // Установить значение элемента с заданным индексом. Если возникнет ошибка, возвратить логическое значение false.
+    public boolean put(int index, int val) {
+        if (indexOK(index)) {                             //Отслеживание попытки выхода  за границы массива
+            a[index] = val;
+            return true;
+        }
+        return false;
+    }
+
+    // Вернуть логическое значение true, если индекс  не выходит за границы массива
+    private boolean indexOK(int index) {
+        return index >= 0 & index < length;
+    }
+}
+
+// Демонстрация работы с "отказоустойчивым" массивом
+public class FSDemo {
+    public static void main(String[] args) {
+        FailSoftArray fs = new FailSoftArray(5, -1);
+        int x;
+
+        // Демонстрация корректной обработки ошибок
+        System.out.println("Oбpaбoткa ошибок без вывода отчета.");
+
+        for (int i = 0; i < (fs.length * 2); i++)
+            fs.put(i, i * 10);  //Для обращения к элементам массива должны использоваться методы доступа
+
+
+        for (int i = 0; i < (fs.length * 2); i++){
+            x = fs.get(i);           //Для обращения к элементам массива должны использоваться методы доступа
+
+        if (x != -1) System.out.print(x + " ");
+        System.out.print("");
+    }
+        //обработать ошибки
+        System.out.println("\nOбpaбoткa ошибок с выводом отчета ");
+
+        for (int i = 0; i < (fs.length * 2); i++)
+            if (!fs.put(i, i * 10))
+                System.out.println("Индекс  " + i + " вне допустимого диапазона");
+
+        for (int i = 0; i < (fs.length * 2); i++) {
+            x = fs.get(i);
+            if (x != -1) System.out.print(x + " ");
+            else
+                System.out.println("Индекс  " + i + " вне допустимого диапазона");
+        }
+    }
+}
--- src/com/page_202.java
+++ src/com/page_202.java
@@ -52,14 +52,38 @@ public class page_202 {
 //        System.out.println();
 
 
-        System.out.println("Mнe нравится Java".length());
-
-        System.out.println("Mнe нравится Java".charAt (2));
-
-
- //       y = x < 0 ? 10 : 20;
+//        System.out.println("Mнe нравится Java".length());
+//
+//        System.out.println("Mнe нравится Java".charAt (2));
 
 
+        //       y = x < 0 ? 10 : 20;
 
+
+        // Нахождение минимального и максимального значений в массиве
+//Finding minimum and maximum values in the array
+        int nums[] = new int[10];
+        int min, max;
+
+        nums[0] = 99;
+        nums[1] = -10;
+        nums[2] = 100123;
+        nums[3] = 18;
+        nums[4] = -978;
+        nums[5] = 5623;
+        nums[6] = 463;
+        nums[7] = -9;
+        nums[8] = 287;
+        nums[9] = 49;
+        min = max = nums[0];
+
+
+        for (int v : nums) {
+            if (v < min) min = v;
+            if (v > max) max = v;
+        }
+        System.out.println("min and max: " + min + " " + max);
     }
 }
+
+
