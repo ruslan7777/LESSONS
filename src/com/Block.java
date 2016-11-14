9d06c80dba1c8a5e03478948242870feef0c8cc4
RUSLAN77 <ruslan01234567890123@gmail.com>
2016-11-14 16:02:27 +0200

    14.11.2016

--- src/com/Block.java
+++ src/com/Block.java
@@ -5,13 +5,37 @@ package com;
  */
 // Методам можно передавать объекты
 class Block {
-    int а, b, c;
+    int a, b, c;
     int volume;
 
     Block(int i, int j, int k) {
-        а = i;
+        a = i;
         b = j;
         c = k;
-        volume = а * b * c;
+        volume = a * b * c;
+    }
+
+    //Вернуть логическое значение true, если параметр оЬ определяет тот же параллелепипед
+    boolean sameBlock(Block ob) {       //Использование объектного типа в качестве параметра
+      /* БЫЛО   if ((ob.a == a) & (ob.b == b) & (ob.c == c)) return true;   else return false;*/
+        return (ob.a == a) & (ob.b == b) & (ob.c == c);
+    }
+
+    //Вернуть логическое значение true, если параметр оЬ определяет параллелепипед того же объема
+    boolean sameVolume(Block ob) {       //Использование объектного типа в качестве параметра
+        /*БЫЛО if (ob.volume == volume) return true;       else return false;*/
+        return ob.volume == volume;
     }
 }
+    class PassOb{
+        public static void main(String[] args) {
+            Block ob1=new Block(10,2,5);
+            Block ob2=new Block(10,2,5);
+            Block ob3=new Block(4,5,5);
+
+            System.out.println("ob1 имеет те же размеры, что и ob2: "+ ob1.sameBlock(ob2)); //Передача объекта
+            System.out.println("ob1 имеет те же размеры, что и ob3: "+ ob1.sameBlock(ob3)); //Передача объекта
+            System.out.println("ob1 имеет тот же объем, что и ob3: "+ ob1.sameVolume(ob3)); //Передача объекта
+        }
+    }
+
